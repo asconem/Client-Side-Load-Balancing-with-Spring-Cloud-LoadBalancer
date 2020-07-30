b756a6559e768928fbe5cf5fd330ee190de600e3
Matthew Ascone <asconem@gmail.com>
2020-07-29 21:50:59 -0400

    Add SayHelloApplicationTests class to say-hello

---
+++ user/src/test/java/hello/SayHelloApplicationTests.java
@@ -0,0 +1,35 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SayHelloApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SayHelloApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void shouldReturn200WhenSendingRequestToRoot() {
+        @SuppressWarnings("rawtypes") ResponseEntity<String> entity = testRestTemplate
+                .getForEntity(
+                        "http://localhost:" + port + "/", String.class);
+
+        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(entity.getBody()).isEqualTo("Hi!");
+    }
+}
